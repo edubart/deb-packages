e8a140e046b2256ad69bc9fa2a31c2c42dd726e4
Eduardo Bart <edub4rt@gmail.com>
2025-01-14 10:47:16 -0300

    Fix apt warning when updating index

--- debian/gen-index.sh
+++ debian/gen-index.sh
@@ -3,7 +3,11 @@ set -e
 cd /apt
 dpkg-scanpackages --multiversion ${REPO_NAME} > ${REPO_NAME}/Packages
 gzip -k -f /apt/${REPO_NAME}/Packages
-cd /apt && apt-ftparchive release ${REPO_NAME} > ${REPO_NAME}/Release
+apt-ftparchive \
+      -o APT::FTPArchive::Release::Origin="Cartesi" \
+      -o APT::FTPArchive::Release::Label="Cartesi APT Repository" \
+      -o APT::FTPArchive::Release::Suite="stable" \
+      release ${REPO_NAME} > ${REPO_NAME}/Release
 gpg -abs -o - /apt/${REPO_NAME}/Release > /apt/${REPO_NAME}/Release.gpg
 gpg --clearsign -o - /apt/${REPO_NAME}/Release > /apt/${REPO_NAME}/InRelease
 echo 'deb $(REPO_URL) ./${REPO_NAME}/' > /apt/${REPO_NAME}/sources.list
